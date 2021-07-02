f9edf2344a545719cff88b1d5b9dc887b9606a32
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-07-02 16:07:04 +0200

    [DEBUG] Print normalized path if it's not absolute

--- libsolidity/interface/FileReader.cpp
+++ libsolidity/interface/FileReader.cpp
@@ -113,6 +113,11 @@ boost::filesystem::path FileReader::normalizeCLIPathForVFS(boost::filesystem::pa
 	// NOTE: boost path preserves certain differences that are ignored by its operator ==.
 	// E.g. "a//b" vs "a/b" or "a/b/" vs "a/b/.". lexically_normal() does remove these differences.
 	boost::filesystem::path normalizedPath =  boost::filesystem::absolute(_path).lexically_normal();
+
+	// TMP:
+	if (!normalizedPath.is_absolute())
+		std::cout << "PROBLEM: '" << _path << "' was normalized into a relative path: " << normalizedPath << std::endl;
+
 	solAssert(normalizedPath.is_absolute(), "");
 
 	// lexically_normal() will not squash paths like "/../../" into "/". We have to do it manually.
